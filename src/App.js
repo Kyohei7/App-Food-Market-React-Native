28025e3556ba0dd4579bab569b50374d22cac300
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-10 11:24:38 +0700

    First Commit & Implementation Atomic Design in Project Structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Food Market App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
+
+export default App
