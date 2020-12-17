b6dba2cf820ea83fa1bec09b8d0a0cd08e639358
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-17 16:49:50 +0700

    Create HomeTabSction Component in Molecules

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,9 +1,11 @@
 import Header from './Header'
 import BottomNavigator from './BottomNavigator'
 import FoodCard from './FoodCard'
+import HomeTabSection from './HomeTabSection'
 
 export {
     Header,
     BottomNavigator,
-    FoodCard
+    FoodCard,
+    HomeTabSection
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,56 +1,9 @@
 import React from 'react'
 import { Image, ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native'
 import { ProfileDummy } from '../../assets'
-import { FoodCard, Gap } from '../../components'
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
-
-const renderTabBar = props => (
-    <TabBar
-      {...props}
-      indicatorStyle={{ 
-          backgroundColor: '#020202',
-          height: 3,
-          width: '15%',
-          marginLeft: '3%' }}
-      style={{ backgroundColor: 'white' }}
-      tabStyle={{ width: 'auto' }}
-      renderLabel={({ route, focused, color }) => (
-        <Text 
-            style={{ 
-                fontFamily: 'Poppins-Medium',
-                color: focused ? '#020202' : '#8D92A3', }}>
-          {route.title}
-        </Text>
-      )}
-    />
-  );
-
-const FirstRoute = () => (
-    <View style={{ flex: 1 }} />
-  );
-   
-const SecondRoute = () => (
-    <View style={{ flex: 1  }} />
-  );
-   
-const initialLayout = { width: Dimensions.get('window').width };
-
+import { FoodCard, Gap, HomeTabSection } from '../../components'
 
 const Home = () => {
-
-    const [index, setIndex] = React.useState(0);
-    const [routes] = React.useState([
-        { key: '1', title: 'New Taste' },
-        { key: '2', title: 'Popular' },
-        { key: '3', title: 'Recommended' },
-    ]);
- 
-    const renderScene = SceneMap({
-        1: FirstRoute,
-        2: SecondRoute,
-        3: FirstRoute,
-    });
-
     return (
         <View style={styles.page} >
             <View style={styles.profileContainer}>
@@ -71,13 +24,7 @@ const Home = () => {
                 </ScrollView>  
             </View>
             <View style={styles.tabContainer}>
-                <TabView
-                    renderTabBar={renderTabBar}
-                    navigationState={{ index, routes }}
-                    renderScene={renderScene}
-                    onIndexChange={setIndex}
-                    initialLayout={initialLayout}
-                />
+                <HomeTabSection />
             </View>
         </View>
     )
