cff74fc1972b351c1973ab6cc232fd1e44b2607f
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-17 16:58:07 +0700

    Create HomeProfile Component in Molecules

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,10 +2,13 @@ import Header from './Header'
 import BottomNavigator from './BottomNavigator'
 import FoodCard from './FoodCard'
 import HomeTabSection from './HomeTabSection'
+import HomeProfile from './HomeProfile'
 
 export {
     Header,
     BottomNavigator,
     FoodCard,
-    HomeTabSection
+    HomeTabSection,
+    HomeProfile
+ 
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Image, ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native'
-import { ProfileDummy } from '../../assets'
-import { FoodCard, Gap, HomeTabSection } from '../../components'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import { FoodCard, Gap, HomeProfile, HomeTabSection } from '../../components'
 
 const Home = () => {
     return (
         <View style={styles.page} >
-            <View style={styles.profileContainer}>
-                <View>
-                    <Text style={styles.appName}>FoodMarket</Text>
-                    <Text style={styles.title} >Let’s get some foods</Text>
-                </View>
-                    <Image source={ProfileDummy} style={styles.profile} />
-            </View>
+            <HomeProfile />
             <View>
                 <ScrollView horizontal showsHorizontalScrollIndicator={false}>
                     <View style={styles.foodCardContainer} >
@@ -36,33 +29,9 @@ const styles = StyleSheet.create({
     page: {
         flex: 1
     },
-    profileContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 24,
-        paddingTop: 32,
-        paddingBottom: 24,
-        backgroundColor:'white'
-    },
     foodCardContainer: {
         flexDirection: 'row',
         marginVertical: 12,
-    },
-    appName: {
-        fontSize: 22,
-        fontFamily: 'Poppins-Medium',
-        color: '#020202'
-    },
-    title: {
-        fontSize: 14,
-        fontFamily: 'Poppins-Light',
-        color: '#020202',
-        
-    },
-    profile: {
-        width: 50,
-        height: 50,
-        borderRadius: 8
     },
     tabContainer: {
         flex: 1
