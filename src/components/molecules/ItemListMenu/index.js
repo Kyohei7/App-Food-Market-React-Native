1c36e34e47b99fb8f4ec983a43c6226563e4b769
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-19 09:15:19 +0700

    Slicing Profile Page

---
+++ src/components/molecules/ItemListMenu/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { IcNext } from '../../../assets';
+
+const ItemListMenu = ({label}) => {
+    return (
+        <View>
+            <TouchableOpacity>
+                <View style={styles.account} >
+                    <Text style={styles.label} >{label}</Text>
+                    <IcNext />
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default ItemListMenu
+
+const styles = StyleSheet.create({
+    account: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 7,
+        alignItems: 'center',
+    
+      },
+      label: {
+        fontSize: 14,
+        fontFamily: 'Poppins-Regular',
+        color: '#020202'
+      }
+})
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,76 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ProfileDummy } from '../../assets'
+import { ProfileTabSection, Gap } from '../../components'
 
 const Profile = () => {
     return (
-        <View>
-            <Text> Profile Screen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.page} >
+                <View style={styles.profileDetail} >
+                    <View style={styles.photo}>
+                        <View style={styles.borderPhoto} >
+                            <Image style={styles.photoContainer} source={ProfileDummy} />
+                        </View>
+                    </View>
+                </View> 
+            <Text style={styles.name} >Muhammad Rizki</Text>
+            <Text style={styles.email}>rizkimuhammad2301@gmail.com</Text>
+            <Gap height={26} />
+                <View style={styles.profileTab} >
+                    <ProfileTabSection />
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
 export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    page: {
+        backgroundColor: 'white',
+        flex: 1
+    },
+    profileTab: {
+        flex: 1
+    },
+    profileDetail: {
+        backgroundColor: 'white',
+        
+    },
+    name: {
+        fontSize: 18,
+        fontFamily: 'Poppins-Medium',
+        color: '#020202',
+        textAlign: 'center'
+    },
+    email: {
+        fontSize: 14,
+        fontFamily: 'Poppins-Light',
+        color: '#8D92A3',
+        textAlign: 'center'
+    },
+    photo: {
+        alignItems: 'center',
+        marginTop: 26,
+        marginBottom: 16
+    },
+    photoContainer: {
+        width: 90,
+        backgroundColor: '#F0F0F0',
+        height: 90,
+        borderRadius: 90,
+        padding: 24,
+    },
+    borderPhoto: {
+        borderWidth: 1,
+        borderColor: '#8D92A3',
+        width: 110,
+        height: 110,
+        borderRadius: 100,
+        borderStyle: 'dashed',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
