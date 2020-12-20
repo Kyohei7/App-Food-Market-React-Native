7baae171bead8c98456a347563ed5b5c2c4e0ee2
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-20 14:57:32 +0700

    Create Submit Form use Local State

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button, Gap, Header, TextInput } from '../../components'
 
 
 const SignIn = ({navigation}) => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmit = () => {
+        console.log('Email ', email)
+        console.log('Password ', password)
+    }
+
     return(
         <View style={styles.page} >
             <Header title="Sign In" subTitle="Find your best ever meal" />
             <View style={styles.container}>
-                <TextInput label="Email Address" placeholder="Type your email address" />
+                <TextInput 
+                    label="Email Address" 
+                    placeholder="Type your email address"
+                    value={email}
+                    onChangeText={(value) => setEmail(value)} />
                 <Gap height={16} />
-                <TextInput label="Password" placeholder="Type your email password" />
+                <TextInput 
+                    label="Password" 
+                    placeholder="Type your email password"
+                    value={password}
+                    onChangeText={(value) => setPassword(value)}
+                    secureTextEntry />
                 <Gap height={24} />
-                <Button text="Sign In" />
+                <Button text="Sign In" onPress={onSubmit}  />
                 <Gap height={12} />
                 <Button 
                     text="Create New Account" 
                     color='#8D92A3' 
-                    textColor='white' 
-                    onPress={() => navigation.navigate('SignUp') } 
+                    textColor='white'
+                    onPress={() => navigation.navigate('SignUp')}
                     />
             </View>
         </View>
